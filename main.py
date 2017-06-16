bc320cdfb3706e0b27dda84e8144b47625c58a55
Jgwentworth <jaredgeorge.us@gmail.com>
2017-06-16 16:43:20 -0700

    finished errors

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ import os
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/", methods=["POST","GET"])
-def index():
-        return render_template('index.html')
-
-@app.route("/")        
+@app.route("/")
+def display_index():
+    return render_template("index.html")    
 
 def is_blank(resp):
     if len(resp) == 0:
@@ -25,7 +23,7 @@ def is_valid(resp):
     else:
         return True                   
 
-@app.route("/welcome", methods=["POST"],)
+@app.route("/", methods=["POST"],)
 def validate_response():
 
     username = request.form["username"]
@@ -40,7 +38,7 @@ def validate_response():
 
     if is_blank(username):
         name_error = "Empty field"
-    else:
+    else:    
         if not is_valid(username):
             name_error = "Not valid (Between 3-20 characters and no whitespace."
             username = ""
@@ -59,14 +57,19 @@ def validate_response():
         elif password != password2:
             pass_error2 = "Not valid (Not matching)"
             password2 = ""
-    if not name_error and not pass_error and not pass2_error:
-                
+    if not name_error and not pass_error and not pass_error2:
+        name = username
+        return redirect("/welcome?name={0}".format(name))        
     else:
         return render_template("index.html",
             name_error = name_error,
             pass_error = pass_error,
             pass_error2 = pass_error2,
-            email_error =email_error
+            email_error = email_error
     )        
+@app.route("/welcome")
+def welcome():
+    name = request.args.get('name')
+    return "<h1>Welcome {0}!</h1>".format(name)    
 
 app.run()        
